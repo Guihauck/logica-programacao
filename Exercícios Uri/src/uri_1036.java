8f9b42b74c0bc8660502fb0ff85bbfd56285aacc
Guilherme Hauck <guihauck1998@gmail.com>
2023-06-09 17:33:05 -0300

    uri 1036 resolvido

---
+++ Exercícios Uri/src/uri_1036.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri_1036 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		Scanner input = new Scanner(System.in);
+
+		double A = input.nextDouble();
+		double B = input.nextDouble();
+		double C = input.nextDouble();
+
+		double delta = Math.pow(B, 2) - 4 * A * C;
+
+		if (A == 0 || delta < 0.00) {
+			System.out.println("Impossivel calcular");
+		}
+
+		else {
+			double R1 = (-B + Math.sqrt(delta)) / (2 * A);
+			double R2 = (-B - Math.sqrt(delta)) / (2 * A);
+			System.out.printf("R1 = %.5f%n", R1);
+			System.out.printf("R2 = %.5f%n", R2);
+		}
+
+		input.close();
+
+	}
+}
