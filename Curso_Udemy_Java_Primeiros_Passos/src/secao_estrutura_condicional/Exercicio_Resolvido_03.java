112006f26a2a310df4775fd46f2abb6f77460139
Guilherme Hauck <guihauck1998@gmail.com>
2023-06-10 13:04:05 -0300

    Inclusão Exercício_resolvido_03

---
+++ Curso_Udemy_Java_Primeiros_Passos/src/secao_estrutura_condicional/Exercicio_Resolvido_03.java
@@ -0,0 +1,28 @@
+package secao_estrutura_condicional;
+
+import java.util.Scanner;
+
+public class Exercicio_Resolvido_03 {
+
+	public static void main(String[] args) {
+
+		Scanner escrevaNumero = new Scanner(System.in);
+
+		int numero1 = escrevaNumero.nextInt();
+		int numero2 = escrevaNumero.nextInt();
+		int numero3 = escrevaNumero.nextInt();
+
+		if (numero1 < numero2 && numero2 < numero3)
+			System.out.println("MENOR: " + numero1);
+		else {
+			if (numero2 < numero3)
+				System.out.println("MENOR: " + numero2);
+			else {
+				System.out.println("MENOR: " + numero3);
+			}
+		}
+
+		escrevaNumero.close();
+	}
+
+}
