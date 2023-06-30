12cb10be84236fd96c2a857afdda5a83203b6912
Guilherme Hauck <guihauck1998@gmail.com>
2023-06-30 20:35:07 -0300

    Teste de mesa 4 e 5

---
+++ curso_programacao/src/testes_de_mesa/Teste_Mesa_5.java
@@ -0,0 +1,18 @@
+package testes_de_mesa;
+
+public class Teste_Mesa_5 {
+	
+	public static void main(String [] args) {
+		
+		int x = 0;
+		
+		while (x < 5) {
+		int y = x * 3;
+		System.out.println(y);
+		x = x +1;
+		}
+		
+		System.out.println("Fim");
+	}
+	
+}
