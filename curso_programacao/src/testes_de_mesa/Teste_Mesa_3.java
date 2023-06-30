446e636e4ec82e748d13dc259f3c1285db86987f
Guilherme Hauck <guihauck1998@gmail.com>
2023-06-30 19:51:19 -0300

    Exercicio de mesa 3

--- curso_programacao/src/testes_de_mesa/Teste_Mesa_3.java
+++ curso_programacao/src/testes_de_mesa/Teste_Mesa_3.java
@@ -1,17 +1,18 @@
 package testes_de_mesa;
 
-public class Teste_Mesa_2 {
+public class Teste_Mesa_3 {
 
 	public static void main(String[] args) {
 
-		int x = 5;
+		int x = 2;
 		int y = 0;
 
-		while (x > 2) {
+		while (x < 60) {
 			System.out.println(x);
-			y = y + x;
-			x = x - 1;
+			x = x * 2;
+			y = y + 10;
 		}
 
 	}
+
 }
---
+++ curso_programacao/src/testes_de_mesa/Teste_Mesa_4.java
@@ -0,0 +1,17 @@
+package testes_de_mesa;
+
+public class Teste_Mesa_4 {
+	
+	public static void main(String[] args) {
+		
+		double x = 100;
+		double y = 100;
+		
+		while (x != y) {
+			System.out.println("olha");
+			x = Math.sqrt(y);
+		}
+		
+	}
+	
+}
