d2fd3e72ba1e0c3e5cc0c9d1a329066d43ff2e56
Guilherme Hauck <guihauck1998@gmail.com>
2023-06-30 19:11:56 -0300

    Teste de mesa 1 e 2

---
+++ curso_programacao/src/testes_de_mesa/Teste_Mesa_2.java
@@ -0,0 +1,17 @@
+package testes_de_mesa;
+
+public class Teste_Mesa_2 {
+	
+	public static void main(String [] args) {
+		
+		int x = 5;
+		int y = 0;
+		
+		while (x > 2) {
+			System.out.println(x);
+			y = y + x;
+			x = x -1;
+		}
+		
+	}
+}
