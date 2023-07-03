93299f747d7f9d3deb29802bc6cc66802421635a
Guilherme Hauck <guihauck1998@gmail.com>
2023-07-03 20:18:32 -0300

    Teste de mesa 6 realizado

---
+++ curso_programacao/src/testes_de_mesa/Teste_Mesa_6.java
@@ -0,0 +1,18 @@
+package testes_de_mesa;
+
+public class Teste_Mesa_6 {
+	public static void main(String[] args) {
+
+		int x = 2;
+		int y = 10;
+
+		System.out.println("Olá");
+
+		while (x < y) {
+			System.out.println(x + "-" + y);
+			x = x * 2;
+			y = y + 1;
+		}
+
+	}
+}
