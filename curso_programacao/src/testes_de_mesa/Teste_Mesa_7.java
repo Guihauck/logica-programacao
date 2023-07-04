285999892f9cc63cab224c15addf48c248cc40ff
Guilherme Hauck <guihauck1998@gmail.com>
2023-07-03 21:20:12 -0300

    Exercicio Resolvido 1 - Estruturas repetitivas

---
+++ curso_programacao/src/testes_de_mesa/Teste_Mesa_7.java
@@ -0,0 +1,22 @@
+package testes_de_mesa;
+
+public class Teste_Mesa_7 {
+
+	public static void main(String[] args) {
+
+		int x = 4;
+		int y = 0;
+		int i = 0;
+
+		while (i < x) {
+
+			i = i + 1;
+			y = y + i;
+
+			System.out.println(i);
+			System.out.println(y);
+
+		}
+
+	}
+}
