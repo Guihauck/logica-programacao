b85d03412555de57b54ab2add71af0b621ad4107
Guilherme Hauck <guihauck1998@gmail.com>
2023-07-24 17:25:54 -0300

    Testes de mesa - estrutura for

---
+++ Secao_Estruturas_Repetitivas/src/Teste_de_mesa/Teste_Mesa_3.java
@@ -0,0 +1,21 @@
+package Teste_de_mesa;
+
+public class Teste_Mesa_3 {
+
+	public static void main(String[] args) {
+
+		int y = 10;
+
+		for (int i = 0; i < 4; i++) {
+
+			System.out.println(i);
+
+			y = y + i;
+
+			System.out.println(y);
+
+		}
+
+	}
+
+}
---
+++ Secao_Estruturas_Repetitivas/src/Teste_de_mesa/Teste_Mesa_4.java
@@ -0,0 +1,20 @@
+package Teste_de_mesa;
+
+public class Teste_Mesa_4 {
+
+	public static void main(String[] args) {
+
+		int x = 4;
+		int y = 0;
+
+		for (int i = 0; i < x; i++) {
+
+			System.out.print(i);
+			System.out.println(x);
+			y = y + 10;
+
+		}
+
+	}
+
+}
