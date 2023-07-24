829f7af8a7145c25f3fccde8db1542df4b43b5d0
Guilherme Hauck <guihauck1998@gmail.com>
2023-07-24 16:07:17 -0300

    Teste de mesa 2 - estrutura for

---
+++ Secao_Estruturas_Repetitivas/src/Teste_de_mesa/Teste_Mesa_2.java
@@ -0,0 +1,18 @@
+package Teste_de_mesa;
+
+public class Teste_Mesa_2 {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<5; i++) {
+			
+			int y = i -1;
+			int x = i * 10;
+			
+			System.out.println(i);
+			
+		}
+		
+	}
+	
+}
